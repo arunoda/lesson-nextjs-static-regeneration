d47711f0da4c2f9faa36476ef0ba750e3fa78f6b
Arunoda Susiripal <50838+arunoda@users.noreply.github.com>
2020-06-28 16:01:52 +0530

    Add the container.

---
+++ components/Container.js
@@ -0,0 +1,48 @@
+export function Container ({children}) {
+    return (
+        <div className="container">
+            {children}
+            <style jsx>{`
+                .container {
+                    text-align: center;
+                    font-family: arial;
+                    margin: auto 10px;
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    bottom: 0;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+
+                .container :global(.music .name) {
+                    font-size: 25px;
+                    font-weight: 600;
+                    margin: 0 0 5px 0;
+                }
+
+                .container :global(.music .image img) {
+                    width: 250px;
+                    height: 250px;
+                }
+
+                .container :global(.music .image) {
+                    margin: 20px 0;
+                }
+
+                .container :global(.reload) {
+                    margin: 10px 0;
+                }
+
+                .container :global(.reload a) {
+                    color: #03A9F4;
+                    text-decoration: none;
+                    padding-bottom: 2px;
+                    border-bottom: 1px solid #03A9F4;
+                }
+            `}</style>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { findSong } from '../lib/find-song'
+import { Container } from '../components/Container';
 
 export default function Home({ song }) {
     return (
-        <div className="container">
+        <Container>
             <div className="music">
                 <div className="name">{song.name}</div>
                 <div className="artist">by {song.artist}</div>
@@ -18,48 +19,7 @@ export default function Home({ song }) {
                     <a href="/">Try Another One</a>
                 </div>
             </div>
-            <style jsx>{`
-                .container {
-                    text-align: center;
-                    font-family: arial;
-                    margin: auto 10px;
-                    position: fixed;
-                    top: 0;
-                    left: 0;
-                    right: 0;
-                    bottom: 0;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-
-                .music .name {
-                    font-size: 25px;
-                    font-weight: 600;
-                    margin: 0 0 5px 0;
-                }
-
-                .music .image img {
-                    width: 250px;
-                    height: 250px;
-                }
-
-                .music .image {
-                    margin: 20px 0;
-                }
-
-                .reload {
-                    margin: 10px 0;
-                }
-
-                .reload a {
-                    color: #03A9F4;
-                    text-decoration: none;
-                    padding-bottom: 2px;
-                    border-bottom: 1px solid #03A9F4;
-                }
-            `}</style>
-        </div>
+        </Container>
     )
 }
 
